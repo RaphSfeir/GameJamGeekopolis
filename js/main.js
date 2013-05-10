c65f669437ad03fea68d2ddb4b5dd158c4d1df9c
RaphSfeir <raphael.sfeir@eleves.ec-nantes.fr>
2013-05-10 19:29:38 +0200

    Debut player.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,69 @@
-var imgMonsterARun = new Image();
 
+
+
+// create spritesheet and assign the associated data.
+jQuery(document).ready(function($) {
+	init(); 
+});
 function init() {
     //find canvas and load images, wait for last image to load
-    cPlayground = document.getElementById("playground");
 
-    imgMonsterARun.onload = handleImageLoad;
-    imgMonsterARun.onerror = handleImageError;
-    imgMonsterARun.src = "../img/sprites/MonsterARun.png";
+    new Player(); 
+	_.Ticker.addListener(window);
+	_.Ticker.useRAF = true;
+	_.Ticker.setFPS(60);
+	_.Ticker.addEventListener("tick", handleTick);
 }
+function debug(data){
+	$('<div>').html(data+'<br/>').prependTo($('#debug'));
+}
+function resize(){
+	$('canvas').each(function(){
+		this.width = window.innerWidth;
+		this.height = window.innerHeight;
+	});
+	//Prevents click. Hud won't be resized ? 
+	//$('#hud').width(window.innerWidth).height(window.innerHeight);
+}
+
+function handleTick() {
+	renderCanvas();
+}
+
+function renderCanvas() {
+	cPlayground.update();
+}
+
+$(document).on('mousemove', function(e){
+	e.preventDefault();
+	var x = e.clientX;
+	var y = e.clientY;
+	mouse.dx = x - mouse.ox;
+	mouse.dy = y - mouse.oy;
+	mouse.x = e.clientX;
+	mouse.y = e.clientY;
+	mouse.ox = x;
+	mouse.oy = y;
+
+});
+$(document).on('mouseup', function(e){
+	e.preventDefault();
+	mouse.x = e.clientX;
+	mouse.y = e.clientY;
+	mouse.down = true;
+	mouse.up = false;
+});
+$(document).on('mousedown', function(e){
+	e.preventDefault();
+	mouse.x = e.clientX;
+	mouse.y = e.clientY;
+	mouse.down = false;
+	mouse.up = true;
+});
+$(window).on('resize', function(){
+	//resize();
+});
+$(document).on('click', function(e){
+	e.preventDefault();
+	return false;
+});
